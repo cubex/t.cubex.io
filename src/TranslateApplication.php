578f37347d638999d7f1b347c2f51e5e0e874993
Brooke Bryan <brooke@bajb.net>
2020-12-17 12:37:20 +0000

    Updated output

--- src/TranslateApplication.php
+++ src/TranslateApplication.php
@@ -27,17 +27,20 @@ class TranslateApplication extends Controller
     return new SafeHtml(
       '<div style="background:#2d2d2d; position: absolute; top:0; left:0; right:0; bottom:0; padding: 10px;">'
       . '<form method="post" action="' . $ctx->request()->path() . '" style="width: 100%; ">'
-      . '<textarea name="text" style="width:100%; min-height:50px; font-size:15px; padding: 20px; '
+      . '<textarea name="text" style="width:100%; min-height:50px; font-size:15px; padding: 15px; '
       . 'background: rgba(255,255,255,0.1); border: 1px solid #454545; color: #c7d7e6">' . $text . '</textarea>'
       . '<input type="submit" value="Translate" '
       . 'style="padding: 10px; width: 100%; border: 1px solid; '
       . 'background: #3c49aa; color: #c4c6da; font-size: 14px; margin-top: 10px;"/>'
       . ($text ?
-        '<p style="color: #CCCCCC; font-size: 12px; font-family: Monaco, SF Mono, monospace; margin: 20px 0 -5px 10px;">Code:</p>'
-        . '<p style="background: #373d44; color: white; padding: 20px; user-select: all;'
-        . 'font-family: Monaco, SF Mono, monospace; font-size:14px;">' . $this->_createTranslatable($key, $text) . '</p>'
-        . '<p style="color: #CCCCCC; font-size: 12px; font-family: Monaco, SF Mono, monospace; margin: 20px 0 -5px 10px;">Key:</p>'
-        . '<p style="background: #373d44; color: white; padding: 20px; user-select: all;'
+        '<p style="color: #aaa; font-size: 10px; font-family: Monaco, SF Mono, monospace; margin: 10px 0 0 0;">CODE:</p>'
+        . '<p style="background: #373d44; color: white; padding: 15px; margin:5px 0; user-select: all;'
+        . 'font-family: Monaco, SF Mono, monospace; font-size:14px;">' . $this->_createTranslatable(
+          $key,
+          $text
+        ) . '</p>'
+        . '<p style="color: #aaa; font-size: 10px; font-family: Monaco, SF Mono, monospace; margin: 10px 0 0 0;">KEY:</p>'
+        . '<p style="background: #373d44; color: white; padding: 15px; margin:5px 0; user-select: all;'
         . 'font-family: Monaco, SF Mono, monospace; font-size:14px;">' . $key . '</p>'
         : '')
       . '</form>'
@@ -59,10 +62,6 @@ class TranslateApplication extends Controller
       }
       $replacements = ', [' . implode(',', $arrVals) . ']';
     }
-    return '$this->_('
-      . "'" . $key . "'"
-      . ", '" . addslashes($text) . "'" .
-      $replacements
-      . ')';
+    return '$this->_(' . "'" . $key . "'" . ", '" . addcslashes($text, "'") . "'" . $replacements . ')';
   }
 }
